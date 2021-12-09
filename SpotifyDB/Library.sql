73b6881047958c37f9c80e0af48a2058853070e7
Christian Jackson <christianjackson13@gmail.com>
2021-12-09 19:49:12 +0100

    Add project files.

---
+++ SpotifyDB/Library.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Library]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [artist] NVARCHAR(MAX) NULL, 
+    [album] NVARCHAR(MAX) NULL, 
+    [track] NVARCHAR(MAX) NULL, 
+    [uri] NVARCHAR(MAX) NULL
+)
